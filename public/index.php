9d3af57348b14621e3c77793ddc9c5ffd7f15c59
Te4g <gaetan.rouseyrol@gmail.com>
2019-10-16 13:28:48 +0200

    qûete terminée

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+require '../vendor/autoload.php';
+
+$loader = new Twig\Loader\FilesystemLoader(__DIR__.'/../src/View');
+$twig = new Twig\Environment($loader);
+
+$products = ['product1', 'product2', 'product3', 'product4', 'product5'];
+
+echo $twig->render('index.html.twig', ['product' => $products]);
+
+
